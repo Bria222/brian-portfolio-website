0bbf677c413916d7e25f7ca306f68288c47a2a63
Bria222 <briantechologies.co.ke@gmail.com>
2022-04-06 12:31:50 +0300

    refactoring javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
-//get humbuger
+/*  eslint linebreak-style: ["error", "windows"]  */
+
+// get humbuger
 const humbuger = document.querySelector('.humbuger');
-//get navigation menu
+// get navigation menu
 const navMenu = document.querySelector('.nav-menu');
 const closeBtn = document.getElementById('close-btn');
 const header = document.querySelector('.mobile-nav');
@@ -8,8 +10,8 @@ const header = document.querySelector('.mobile-nav');
 humbuger.addEventListener('click', () => {
   humbuger.classList.add('active');
   navMenu.classList.add('active');
-  
-  //display mobile nav
+
+  // display mobile nav
   header.style.cssText = `
   background-color: rgba(96, 112, 255, 0.85);
   position: fixed;
@@ -17,9 +19,9 @@ humbuger.addEventListener('click', () => {
   left: 0;
   width: 100%;
   height: 100vh;
-  `
-  closeBtn.style.display = 'block'
-})
+  `;
+  closeBtn.style.display = 'block';
+});
 
 closeBtn.addEventListener('click', () => {
   humbuger.classList.remove('active');
@@ -29,21 +31,17 @@ closeBtn.addEventListener('click', () => {
     position: fixed;
     top: -9999px;
     left: -9999px;
-    `
-   
-})
+    `;
+});
 
-document.querySelectorAll('.nav-link').forEach((n) =>
-  n.addEventListener('click', () => {
-    humbuger.classList.remove('active');
-    navMenu.classList.remove('active');
-   
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  humbuger.classList.remove('active');
+  navMenu.classList.remove('active');
 
-    header.style.cssText = `
+  header.style.cssText = `
     background-color: rgba(96, 112, 255, 0.85);
     position: fixed;
     top: -9999px;
     left: -9999px;
-    `
-  }),
-);
+    `;
+}));
