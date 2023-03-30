8756e27f8f53bc56bafd58650ae8d8b6f15463c1
Bria222 <techconsultants.bree@gmail.com>
2023-03-30 11:42:37 +0300

    update resume

--- db/dist/projects.js
+++ db/dist/projects.js
@@ -3,160 +3,149 @@ exports.__esModule = true;
 // Image dimension ratios => 1.75
 var projects = [
     {
-        title: 'Amazon Clone',
-        note: 'A full-stack e-commerce website inspired by the Amazon website',
-        image: '/projects/amazon-clone.webp',
-        imgDims: { width: '2760', height: '1577' },
-        description: 'A Responsive Full-stack e-commerce website inspired by <a href="https://amazon.com" rel="noreferrer noopener" target="_blank">the amazon website</a>. Built with React, Redux, and Firebase. Including functionalities such as Creating/Signing in to an account, Adding items to the cart, Making payments and so on',
-        stack: ['React', 'Redux', 'Firebase'],
-        links: {
-            github: 'https://github.com/okoyecharles/amazon-clone',
-            live: 'https://clone-f50ae.web.app'
-        }
-    },
-    // {
-    //   title: 'Recipe App',
-    //   note: 'A full-stack application that allows users to create and manage recipes',
-    //   image: '/projects/recipe-app.webp',
-    //   imgDims: { width: '2465', height: '1409' },
-    //   description: 'The Recipe app keeps track of all your recipes, ingredients, and inventory. It will allow you to save ingredients, keep track of what you have, create recipes, and generate a shopping list based on what you have and what you are missing from a recipe.',
-    //   stack: ['Rails', 'Ruby', 'JavaScript'],
-    //   links: {
-    //     github: 'https://github.com/okoyecharles/rails-recipe-app',
-    //     live: 'https://frozen-mesa-28960.herokuapp.com/',
-    //   },
-    // },
-    {
-        title: 'Lavish Cuisine',
-        note: 'A meal-based website that gives you all the information you need about your favourite meals',
-        image: '/projects/lavish-cuisine.webp',
-        imgDims: {
-            width: '2377',
-            height: '1358'
-        },
-        description: 'A meal-based website that gives you all the information you need about your favourite breakfast, lunch and dinner. built with react-typescript & redux.',
-        stack: ['React', 'Redux', 'TypeScript'],
-        links: {
-            github: 'https://github.com/okoyecharles/lavish-cuisine',
-            live: 'https://lavish-cuisine.web.app'
-        }
+      title: 'Prime Shows HD',
+      note: 'Prime Shows streaming software used to track and watch online movies and tv series.',
+      image: '/projects/hdshows.png',
+      imgDims: { width: '2496 ', height: '1427' },
+      description: 'Microverse Second Capstone Project: This educational project is to build our own web application based on an external API. We selected an API that provides data about a topic that we like and built the web app around it. The web app will have 2 or 3 user interfaces (depending on the size of our team',
+      stack: ['Next.js', 'Redux', 'Express', 'MongoDB'],
+      links: {
+        github: 'https://github.com/Bria222/movie-TVmaze-API/',
+        live: 'https://prime-shows.netlify.app/',
+      },
     },
     {
-        title: 'Nethub',
-        note: 'A Movie application that extracts information from the TV Maze API',
-        image: '/projects/nethub.webp',
-        imgDims: {
-            width: '2337',
-            height: '1335'
-        },
-        description: 'A Movie website that displays an arranged list of movies and related information on each one. A place to go when you need to get extra info and discuss with others on a movie.',
-        stack: ['Webpack', 'JavaScript', 'TV Maze API'],
-        links: {
-            github: 'https://github.com/okoyecharles/Nethub',
-            live: 'https://okoyecharles.github.io/Nethub'
-        }
-    },
-    // {
-    //   title: 'Wallet Detective',
-    //   note: 'A full-stack application where you can manage your budget',
-    //   image: '/projects/wallet-inspector.webp',
-    //   imgDims: { width: '2742', height: '1576' },
-    //   description: 'A full-stack application where you can manage your budget: you have a list of payments associated with a category, so you can see how much money you spent and on what',
-    //   stack: ['Rails', 'Ruby', 'Javascript'],
-    //   links: {
-    //     github: 'https://github.com/okoyecharles/rails-budget-app',
-    //     live: 'https://sleepy-hamlet-84522.herokuapp.com/',
-    //   },
-    // },
-    {
-        title: 'Save A Child',
-        note: 'A website focused on preserving the life of sick children',
-        image: '/projects/save-a-child.webp',
-        imgDims: {
-            width: '2337',
-            height: '1335'
-        },
-        description: 'Save The Child is a project I built around the theme of a non-profit organization brought together solely for giving the youth from different continents who have been inflicted mentally, physically or psychologically by the COVID-19 Pandemeic.',
-        stack: ['Webpack', 'JavaScript'],
-        links: {
-            github: 'https://github.com/okoyecharles/save-a-child',
-            live: 'https://okoyecharles.github.io/save-a-child'
-        }
+      title: 'Motorbike Booking',
+      note: 'A full-stack Motorbike Booking website inspired by the Uber website',
+      image: '/projects/mock-motobike.png',
+      imgDims: { width: '1380', height: '788.5' },
+      description: 'Motorbike Booking is an application enables a user to view available motorbikes and reserve a specific one, as well as allows an admin to create, delete and add new categories of motorbikes..',
+      stack: ['React', 'Redux', 'JavaScript', 'Ruby', 'API', 'Ruby on rails'],
+      links: {
+        github: 'https://github.com/Bria222/Motorbike-Booking-capstone',
+        live: 'https://motorbike-booking.netlify.app/',
+      },
     },
     {
-        title: 'Todo List',
-        note: 'A Todo list website that saves user\'s tasks in Local Storage',
-        image: '/projects/todo-list.webp',
-        imgDims: {
-            width: '2345',
-            height: '1340'
-        },
-        description: 'A Todo list website created with HTML, Sass and JavaScript that saves user\'s tasks in Local Storage. This app also allows swapping/reordering of tasks implemented utilizing the Javascript Drag & Drop API',
-        stack: ['JavaScript'],
-        links: {
-            github: 'https://github.com/okoyecharles/Todo-List',
-            live: 'https://okoyecharles.github.io/Todo-Lis'
-        }
+      title: 'Cimeza Africa',
+      note: 'A digital Marketing Agency website that gives you all the information you need about digital marketing solutions',
+      image: '/projects/cimeza.png',
+      imgDims: {
+        width: '2377',
+        height: '1358',
+      },
+      description: 'cimeza africa project is to build a mobile  and web application for a digital marketing agency using React and Redux..',
+      stack: ['React', 'Redux', 'TypeScript'],
+      links: {
+        github: 'https://github.com/Bria222/Bree-collection',
+        live: 'https://cimeza-africa.com',
+      }
     },
     {
-        title: 'Portfolio v1',
-        note: 'A website where you can learn about Okoye Charles and what he does',
-        image: '/projects/portolio-v1.webp',
-        imgDims: {
-            width: '2751 ',
-            height: '1572'
-        },
-        description: 'This is the first version of my portfolio, it served as a collection of my most recent, expertly crafted, and innovative projects, as well as a base of knowledge concerning the things I do.',
-        stack: ['React', 'TypeScript', 'Sass'],
-        links: {
-            github: 'https://github.com/okoyecharles/portfolio',
-            live: 'https://okoyecharles.netlify.app'
-        }
+      title: 'Bookstore CMS',
+      note: 'A bookstore CMS application that displays books information the bookstore API',
+      image: '/projects/mock_bookstore.png',
+      imgDims: {
+        width: '2337',
+        height: '1335',
+      },
+      description: 'Microverse Module 03 Project: This educational project is similar to the "Awesome Books" website. It is a MVP version of it that allows you to display a list of books, add a book and remove a selected book.',
+      stack: ['React', 'Redux', 'Router', 'JavaScript', 'Jest', 'Sass'],
+      links: {
+        github: 'https://github.com/Bria222/react-bookstore',
+        live: 'https://6425442a6752af422aef4ead--brian-nyachae-react-bookstore.netlify.app/',
+      },
+    },
+    
+    {
+      title: 'Math Magicians',
+      note: 'A website focused on performing methemathical problems',
+      image: '/projects/mock_math.png',
+      imgDims: {
+        width: '2337',
+        height: '1335',
+      },
+      description: 'Microverse Module 03 Project: "Math Magicians" is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to make a simple calculations and read a random math-related quote.',
+      stack: ['React','JavaScript', 'Jest', 'Sass'],
+      links: {
+        github: 'https://github.com/Bria222/React-Math-magicians',
+        live: 'https://bree-math-magician.netlify.app/',
+      },
     },
     {
-        title: 'Space Traveler Hub',
-        note: 'A web application that provides commercial and scientific space travel services',
-        image: '/projects/space-traveler-hub.webp',
-        imgDims: {
-            width: '2835',
-            height: '1620'
-        },
-        description: 'A web application for a company that provides commercial and scientific space travel services, utilizing information from the SpaceX API.',
-        stack: ['React', 'Redux', 'SpaceX API'],
-        links: {
-            github: 'https://github.com/Bria222/space-travelers-hub',
-            live: 'https://brian-nyachae-space-travellers.netlify.app/'
-        }
+      title: 'Todo List',
+      note: 'A Todo list website that saves user\'s tasks in Local Storage',
+      image: '/projects/todo-list.webp',
+      imgDims: {
+        width: '2345',
+        height: '1340',
+      },
+      description: 'A Todo list website created with HTML, Sass and JavaScript that saves user\'s tasks in Local Storage. This app also allows swapping/reordering of tasks implemented utilizing the Javascript Drag & Drop API',
+      stack: ['Webpack', 'JavaScript', 'Linters', 'Sass'],
+      links: {
+        github: 'https://github.com/Bria222/Sneak-peek-To-do-list',
+        live: 'https://bria222.github.io/Sneak-peek-To-do-list/',
+      },
     },
     {
-        title: 'Decrypt',
-        note: 'A crypto-based application that displays real-time data on cryptocurrencies and stocks',
-        image: '',
-        imgDims: {
-            width: '',
-            height: ''
-        },
-        description: 'A crypto-API based mobile application that displays real-time data on all crypto-currencies and stocks, also including a display page that gets main information for a single crypto currency.',
-        stack: ['React', 'Redux', 'Sass'],
-        links: {
-            github: 'https://github.com/okoyecharles/decrypt',
-            live: 'https://charles-decrypt.netlify.app'
-        }
+      title: 'Events 254 Conference 2023',
+      note: 'A website where you can view and book online events',
+      image: '/projects/events254.png',
+      imgDims: {
+        width: '2751 ',
+        height: '1572',
+      },
+      description: 'Microverse First Capstone Project: This is an educational project to make a website for a web conference event with home page and about page.',
+      stack: ['React', 'TypeScript', 'Sass'],
+      links: {
+        github: 'https://github.com/Bria222/Capstone.git',
+        live: 'https://bria222.github.io/Capstone/'
+      }
     },
     {
-        title: 'Type Effect Library',
-        note: 'A light-weight, open-source typing-effect library that enables potential users to add a typing effect to mere DOM elements',
-        image: '',
-        imgDims: {
-            width: '',
-            height: ''
-        },
-        descrription: 'A light-weight, open-source typing-effect library that enables potential users to add a typing effect to mere DOM elements, including features for optimizing typing behavior and speed',
-        stack: ['JavaScript', 'HTML5'],
-        links: {
-            github: 'https://github.com/okoyecharles/Type-Effect-Library',
-            live: 'https://okoyecharles.github.io/Type-Effect-Library'
-        }
-    }
-];
+      title: 'Space Traveler Hub',
+      note: 'A web application that provides commercial and scientific space travel services',
+      image: '/projects/space-traveler-hub.webp',
+      imgDims: {
+        width: '2835',
+        height: '1620'
+      },
+      description: 'A web application for a company that provides commercial and scientific space travel services, utilizing information from the SpaceX API.',
+      stack: ['React', 'Redux', 'SpaceX API'],
+      links: {
+        github: 'https://github.com/Bria222/space-travelers-hub.git',
+        live: 'https://app-space-travelers-hub.netlify.app/'
+      }
+    },
+    {
+      title: 'Leaderboard',
+      note: 'This is an educational project to create a website which displays scores submitted by different players.',
+      image: '/projects/mock_ldb.png',
+      imgDims: {
+        width: '2300',
+        height: '1314'
+      },
+      description: 'This is an educational project to create a website which displays scores submitted by different players. It also allows you to submit your score. All data is preserved thanks to the external Leaderboard API Service.',
+      stack: ['JavaScript', 'HTML5'],
+      links: {
+        github: 'https://github.com/Bria222/Leaderboard',
+        live: 'https://mavericks-db.github.io/leaderboard/dist/index.html'
+      }
+    },
+    {
+      title: 'Crypto Currency Web App',
+      note: 'The application allow users to get full information related to the crypto.',
+      image: '/projects/crypto.png',
+      imgDims: {
+        width: '2880',
+        height: '1645'
+      },
+      description: '"Digital Currency" is about working with the real live data from the Blockchain API. It\'s a web application for a company that provides informations such as trending coins, coins\' price,and crypto transaction. The application allow users to get full information related to the crypto.',
+      stack: ['React', 'Redux', 'Sass'],
+      links: {
+        github: 'https://github.com/Bria222/crypto_currency.',
+        live: 'https://brian-nyachae-capstone-project.netlify.app/'
+      }
+    },
+  ];
 exports["default"] = projects;
