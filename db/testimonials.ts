4b4788f40cde5c48e1936a4297ffa7e5619b23a6
Bria222 <techconsultants.bree@gmail.com>
2023-03-30 15:59:31 +0300

    update recommendation refs

--- db/testimonials.ts
+++ db/testimonials.ts
@@ -3,7 +3,7 @@ const testimonials = [
     author: 'Aleksandra Ujvari',
     image: 'Aleksandra.jpeg',
     text: [
-      "Charles is a phenomenal developer; his approach to technical problems is incredible. Charles is a developer that every company wants to have on their team.",
+      "Brian is a phenomenal developer; his approach to technical problems is incredible. Brian is a developer that every company wants to have on their team.",
       "We are from different countries and cultures, but we communicated effectively without blockers. If you are looking for a developer with both technical and soft skills, don't hesitate to contact him."
     ],
     location: 'Vojvodina, Serbia',
@@ -13,7 +13,7 @@ const testimonials = [
     author: 'Clifford Owusu',
     image: 'Clifford.jpeg',
     text: [
-      "Charles and I worked together on a couple of projects, and I appreciate his skills and discipline. He consistently gave 100% effort to our work and played a significant role in ensuring that we completed assignments on time.",
+      "Brian and I worked together on a couple of projects, and I appreciate his skills and discipline. He consistently gave 100% effort to our work and played a significant role in ensuring that we completed assignments on time.",
       "He has the focus and skills that keep him and his team productive during intense crunch periods. I couldn’t recommend him more for any business looking for new talent."],
     location: 'Accra, Ghana',
     link: 'https://linkedin.com/in/sireclifford'
@@ -21,7 +21,7 @@ const testimonials = [
   {
     author: 'Dorian Urem',
     image: 'Dorian.jpeg',
-    text: ["Charles is a great guy. His attitude and approach to work are really good. I would love to have him as a part of my team since he is hard-working, smart, and also technically competent. To see a young individual as driven as him is inspiring. He definitely has a bright future ahead of him."],
+    text: ["Brian is a great guy. His attitude and approach to work are really good. I would love to have him as a part of my team since he is hard-working, smart, and also technically competent. To see a young individual as driven as him is inspiring. He definitely has a bright future ahead of him."],
     location: 'Dubrovnik, Croatia',
     link: 'https://linkedin.com/in/dorian-urem'
   },
@@ -29,8 +29,8 @@ const testimonials = [
     author: 'Elizabeth Ojesanmi',
     image: 'Elizabeth.jpeg',
     text: [
-      "When I think about Charles, the words “hardworking and innovative” spring to mind. Okoye Charles is someone I've had the pleasure of knowing, and we get along great.",
-      "Charles is a great guy for any position needing a full-stack developer and has my highest recommendation."
+      "When I think about Brian, the words “hardworking and innovative” spring to mind. Okoye Brian is someone I've had the pleasure of knowing, and we get along great.",
+      "Brian is a great guy for any position needing a full-stack developer and has my highest recommendation."
     ],
     location: 'Lagos, Nigeria',
     link: 'https://linkedin.com/in/oyinlade-ojesanmi'
@@ -39,8 +39,8 @@ const testimonials = [
     author: 'Ishpaul Singh',
     image: 'Ishpaul.png',
     text: [
-      "Charles is very knowledgeable when it comes to working with software tools and technologies, and he is also a very fast learner. I really enjoyed our pair-programming sessions together, as he was a helpful and understanding team member.",
-      "I would recommend Okoye Charles for immediate full-stack jobs as he can also handle pressure well."
+      "Brian is very knowledgeable when it comes to working with software tools and technologies, and he is also a very fast learner. I really enjoyed our pair-programming sessions together, as he was a helpful and understanding team member.",
+      "I would recommend Okoye Brian for immediate full-stack jobs as he can also handle pressure well."
     ],
     location: 'New Delhi, India',
     link: 'https://linkedin.com/in/ishpaul777'
@@ -49,7 +49,7 @@ const testimonials = [
     author: 'Mohamed Aachour',
     image: 'Mohamed.jpeg',
     text: [
-      "Charles Okoye is one of the best people I have ever met and is a loyal colleague who understands complex matters.",
+      "Brian Okoye is one of the best people I have ever met and is a loyal colleague who understands complex matters.",
       "He is a strong and goal-oriented team player; with every problem, there was a solution. Highly recommended."
     ],
     location: 'Kenitra, Morocco',
@@ -59,8 +59,8 @@ const testimonials = [
     author: 'Sidney Kaguli',
     image: 'Sidney.jpeg',
     text: [
-      "I had the pleasure of working with Charles on a three-week project (with 3 other members), a full-stack reservation app with Rails and React.",
-      "I was particularly impressed by Charles' ability to handle challenges, whether it's getting to the root of buggy code or getting people on board with ideas."
+      "I had the pleasure of working with Brian on a three-week project (with 3 other members), a full-stack reservation app with Rails and React.",
+      "I was particularly impressed by Brian' ability to handle challenges, whether it's getting to the root of buggy code or getting people on board with ideas."
     ],
     location: 'Nairobi, Kenya',
     link: 'https://linkedin.com/in/sidney-kaguli'
@@ -69,7 +69,7 @@ const testimonials = [
     author: 'Tracey Kadenyi',
     image: 'Tracey.jpeg',
     text: [
-      "There are so many good things I can say about working with Okoye Charles. A high note for me was how impressive his technical skills are. He has expert knowledge in JavaScript, React.js, and Rails.",
+      "There are so many good things I can say about working with Okoye Brian. A high note for me was how impressive his technical skills are. He has expert knowledge in JavaScript, React.js, and Rails.",
       "He is always willing to help a teammate facing a code blocker without being condescending about it. Such generosity with knowledge and skill is what makes him easy and fun to work with. Trust me, you'd be lucky to have him on your team."
     ],
     location: 'Nairobi County, Kenya',
