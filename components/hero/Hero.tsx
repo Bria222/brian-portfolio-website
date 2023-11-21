d55ee9ac7f16fac534093b852c1700bda3c676ce
Bria222 <briatechologies@gmail.com>
2023-11-21 23:46:14 +0300

    add one project on stack

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect } from "react";
-import Section from "../main/Section";
-import { useTrail, a, config } from "@react-spring/web";
+import React, { useEffect } from 'react'
+import Section from '../main/Section'
+import { useTrail, a, config } from '@react-spring/web'
 
 interface heroProps {
-  heroRef?: React.MutableRefObject<null>;
+  heroRef?: React.MutableRefObject<null>
 }
 
 const Hero: React.FC<heroProps> = ({ heroRef }) => {
@@ -16,7 +16,7 @@ const Hero: React.FC<heroProps> = ({ heroRef }) => {
       tension: 235,
       friction: 28,
     },
-  }));
+  }))
 
   // Start hero animation after a second
   useEffect(() => {
@@ -24,25 +24,27 @@ const Hero: React.FC<heroProps> = ({ heroRef }) => {
       api.start({
         y: 0,
         opacity: 1,
-      });
-    }, 1000);
-  }, []);
+      })
+    }, 1000)
+  }, [])
 
   return (
-    <Section label="hero" id="content" sectionRef={heroRef}>
+    <Section label='hero' id='content' sectionRef={heroRef}>
       <a.h1 style={springs[0]}>Hi, my name is</a.h1>
       <a.h2 style={springs[0]}>Brian Nyachae.</a.h2>
       <a.h3 style={springs[1]}>I embrace the digital world.</a.h3>
       <a.p style={springs[2]}>
-      I’m a Full-stack web developer specializing in building (and occasionally designing) exceptional digital experiences.
-       I use different technologies such as Javascript, React, Redux, Node.js and Ruby on Rails to transform ideas into real-world
-       software applications. I am available for hire.
+        I’m a Full-stack Software Engineer specializing in building (and
+        occasionally designing) exceptional digital experiences. I use different
+        technologies such as Javascript, React, Redux, Node.js,python and Ruby
+        on Rails to transform ideas into real-world software applications. I am
+        available for hire.
       </a.p>
-      <a.a href="#featured" style={springs[3]}>
+      <a.a href='#featured' style={springs[3]}>
         <button tabIndex={-1}>Check out my work!</button>
       </a.a>
     </Section>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
--- db/projects.ts
+++ db/projects.ts
@@ -1,11 +1,25 @@
 // Image dimension ratios => 1.75
 const projects = [
   {
+    title: 'Payhero Kenya',
+    note: 'Pay Hero Kenya: Accept Mobile Money Payments in Kenya.',
+    image: '/projects/payhero.png',
+    imgDims: { width: '2496 ', height: '1427' },
+    description:
+      'Payhero is a solution to empower businesses to streamline operations, achieve sustainable growth, and enhance customer communication. With our diverse range of tools such as payment APIs, WhatsApp gateway, e-commerce plugins, email APIs, callback as a service, scheduler service, and developer APIs, businesses can effortlessly accept payments, manage finances, and engage with customers.',
+    stack: ['Next.js', 'Redux', 'Express', 'MongoDB', 'Golang'],
+    links: {
+      github: 'https://github.com/Bria222/',
+      live: 'https://payherokenya.com/sps/',
+    },
+  },
+  {
     title: 'Prime Shows HD',
     note: 'Prime Shows streaming software used to track and watch online movies and tv series.',
     image: '/projects/hdshows.png',
     imgDims: { width: '2496 ', height: '1427' },
-    description: 'Microverse Second Capstone Project: This educational project is to build our own web application based on an external API. We selected an API that provides data about a topic that we like and built the web app around it. The web app will have 2 or 3 user interfaces (depending on the size of our team',
+    description:
+      'Microverse Second Capstone Project: This educational project is to build our own web application based on an external API. We selected an API that provides data about a topic that we like and built the web app around it. The web app will have 2 or 3 user interfaces (depending on the size of our team',
     stack: ['Next.js', 'Redux', 'Express', 'MongoDB'],
     links: {
       github: 'https://github.com/Bria222/movie-TVmaze-API/',
@@ -17,7 +31,8 @@ const projects = [
     note: 'A full-stack Motorbike Booking website inspired by the Uber website',
     image: '/projects/mock-motobike.png',
     imgDims: { width: '1380', height: '788.5' },
-    description: 'Motorbike Booking is an application enables a user to view available motorbikes and reserve a specific one, as well as allows an admin to create, delete and add new categories of motorbikes..',
+    description:
+      'Motorbike Booking is an application enables a user to view available motorbikes and reserve a specific one, as well as allows an admin to create, delete and add new categories of motorbikes..',
     stack: ['React', 'Redux', 'JavaScript', 'Ruby', 'API', 'Ruby on rails'],
     links: {
       github: 'https://github.com/Bria222/Motorbike-Booking-capstone',
@@ -32,12 +47,13 @@ const projects = [
       width: '2377',
       height: '1358',
     },
-    description: 'cimeza africa project is to build a mobile  and web application for a digital marketing agency using React and Redux..',
+    description:
+      'cimeza africa project is to build a mobile  and web application for a digital marketing agency using React and Redux..',
     stack: ['React', 'Redux', 'TypeScript'],
     links: {
       github: 'https://github.com/Bria222/Bree-collection',
       live: 'https://cimeza-africa.com',
-    }
+    },
   },
   {
     title: 'Bookstore CMS',
@@ -47,14 +63,15 @@ const projects = [
       width: '2337',
       height: '1335',
     },
-    description: 'Microverse Module 03 Project: This educational project is similar to the "Awesome Books" website. It is a MVP version of it that allows you to display a list of books, add a book and remove a selected book.',
+    description:
+      'Microverse Module 03 Project: This educational project is similar to the "Awesome Books" website. It is a MVP version of it that allows you to display a list of books, add a book and remove a selected book.',
     stack: ['React', 'Redux', 'Router', 'JavaScript', 'Jest', 'Sass'],
     links: {
       github: 'https://github.com/Bria222/react-bookstore',
       live: 'https://6425442a6752af422aef4ead--brian-nyachae-react-bookstore.netlify.app/',
     },
   },
-  
+
   {
     title: 'Math Magicians',
     note: 'A website focused on performing methemathical problems',
@@ -63,8 +80,9 @@ const projects = [
       width: '2337',
       height: '1335',
     },
-    description: 'Microverse Module 03 Project: "Math Magicians" is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to make a simple calculations and read a random math-related quote.',
-    stack: ['React','JavaScript', 'Jest', 'Sass'],
+    description:
+      'Microverse Module 03 Project: "Math Magicians" is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to make a simple calculations and read a random math-related quote.',
+    stack: ['React', 'JavaScript', 'Jest', 'Sass'],
     links: {
       github: 'https://github.com/Bria222/React-Math-magicians',
       live: 'https://bree-math-magician.netlify.app/',
@@ -72,13 +90,14 @@ const projects = [
   },
   {
     title: 'Todo List',
-    note: 'A Todo list website that saves user\'s tasks in Local Storage',
+    note: "A Todo list website that saves user's tasks in Local Storage",
     image: '/projects/todo-list.webp',
     imgDims: {
       width: '2345',
       height: '1340',
     },
-    description: 'A Todo list website created with HTML, Sass and JavaScript that saves user\'s tasks in Local Storage. This app also allows swapping/reordering of tasks implemented utilizing the Javascript Drag & Drop API',
+    description:
+      "A Todo list website created with HTML, Sass and JavaScript that saves user's tasks in Local Storage. This app also allows swapping/reordering of tasks implemented utilizing the Javascript Drag & Drop API",
     stack: ['Webpack', 'JavaScript', 'Linters', 'Sass'],
     links: {
       github: 'https://github.com/Bria222/Sneak-peek-To-do-list',
@@ -93,12 +112,13 @@ const projects = [
       width: '2751 ',
       height: '1572',
     },
-    description: 'Microverse First Capstone Project: This is an educational project to make a website for a web conference event with home page and about page.',
+    description:
+      'Microverse First Capstone Project: This is an educational project to make a website for a web conference event with home page and about page.',
     stack: ['React', 'TypeScript', 'Sass'],
     links: {
       github: 'https://github.com/Bria222/Capstone.git',
-      live: 'https://bria222.github.io/Capstone/'
-    }
+      live: 'https://bria222.github.io/Capstone/',
+    },
   },
   {
     title: 'Space Traveler Hub',
@@ -106,14 +126,15 @@ const projects = [
     image: '/projects/space-traveler-hub.webp',
     imgDims: {
       width: '2835',
-      height: '1620'
+      height: '1620',
     },
-    description: 'A web application for a company that provides commercial and scientific space travel services, utilizing information from the SpaceX API.',
+    description:
+      'A web application for a company that provides commercial and scientific space travel services, utilizing information from the SpaceX API.',
     stack: ['React', 'Redux', 'SpaceX API'],
     links: {
       github: 'https://github.com/Bria222/space-travelers-hub.git',
-      live: 'https://app-space-travelers-hub.netlify.app/'
-    }
+      live: 'https://app-space-travelers-hub.netlify.app/',
+    },
   },
   {
     title: 'Leaderboard',
@@ -121,14 +142,15 @@ const projects = [
     image: '/projects/mock_ldb.png',
     imgDims: {
       width: '2300',
-      height: '1314'
+      height: '1314',
     },
-    description: 'This is an educational project to create a website which displays scores submitted by different players. It also allows you to submit your score. All data is preserved thanks to the external Leaderboard API Service.',
+    description:
+      'This is an educational project to create a website which displays scores submitted by different players. It also allows you to submit your score. All data is preserved thanks to the external Leaderboard API Service.',
     stack: ['JavaScript', 'HTML5'],
     links: {
       github: 'https://github.com/Bria222/Leaderboard',
-      live: 'https://mavericks-db.github.io/leaderboard/dist/index.html'
-    }
+      live: 'https://mavericks-db.github.io/leaderboard/dist/index.html',
+    },
   },
   {
     title: 'Crypto Currency Web App',
@@ -136,17 +158,18 @@ const projects = [
     image: '/projects/crypto.png',
     imgDims: {
       width: '2880',
-      height: '1645'
+      height: '1645',
     },
-    description: '"Digital Currency" is about working with the real live data from the Blockchain API. It\'s a web application for a company that provides informations such as trending coins, coins\' price,and crypto transaction. The application allow users to get full information related to the crypto.',
+    description:
+      '"Digital Currency" is about working with the real live data from the Blockchain API. It\'s a web application for a company that provides informations such as trending coins, coins\' price,and crypto transaction. The application allow users to get full information related to the crypto.',
     stack: ['React', 'Redux', 'Sass'],
     links: {
       github: 'https://github.com/Bria222/crypto_currency.',
-      live: 'https://brian-nyachae-capstone-project.netlify.app/'
-    }
+      live: 'https://brian-nyachae-capstone-project.netlify.app/',
+    },
   },
-];
+]
 
-export type projectType = typeof projects[0];
+export type projectType = (typeof projects)[0]
 
 export default projects
